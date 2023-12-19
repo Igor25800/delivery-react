cdad051113d820f9cf0c6d420f280d25d5aac111
Igor <73959549+Igor25800@users.noreply.github.com>
2023-12-20 01:19:36 +0200

    fix css

--- src/components/navLeft/navLeft.tsx
+++ src/components/navLeft/navLeft.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './navLeft.module.scss'
 import {useGetCategoryQuery} from "../../store/services/CategoryApi";
 import {ICategory} from "../../shared/interfaces/category/category";
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const NavLeft = () => {
     const {data} = useGetCategoryQuery();
@@ -12,12 +12,10 @@ const NavLeft = () => {
             <div className={style.navLeft}>
                 {data?.map((item: ICategory, ind: number) =>
                     <div key={ind} className={style.card}>
-                        <div className={style.item}>
-                            <img className={style.img}
-                                 src={item.file}
-                                 alt=''/>
-                            <NavLink to={`/menu/${item.category}`} className={style.visible}>{item.category}</NavLink>
-                        </div>
+                        <img className={style.img}
+                             src={item.file}
+                             alt=''/>
+                        <NavLink to={`/menu/${item.category}`} className={style.visible}>{item.category}</NavLink>
                     </div>
                 )}
             </div>
